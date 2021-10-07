11adbb491a310bebec560ea01c1843b61c13255d
Humble-Preacher <61693488+Humble-Preacher@users.noreply.github.com>
2021-10-07 08:52:22 +0000

    merging

--- index.js
+++ index.js
@@ -1,45 +1,48 @@
 const express = require('express')
 const app = express()
-const products = [
-    {
-        id: 1,
-        name: "iphone 13 pro max",
-        category: "Electronis",
-        price: 9900,
-        currency: "USD",
-        images: "",
-        Stars: 4,
-        review: "This is an amazing product"
-    },
-    {
-        id: 2,
-        name: "Butter Bread",
-        category: "Food",
-        price: 5,
-        currency: "USD",
-        images: "",
-        Stars: 5,
-        review: "This is onre of the breadon the merket"
-    }
-];
 
-const customers = [
-    {
-        id: 1,
-        name: "Regan Kofi Marfo",
-        gender: "Male"
-    },
-    {
-        id: 2,
-        name: "Kwame Nkrumah",
-        gender: "Male"
-    },
-    {
-        id: 1,
-        name: "Yaa Asantewaa",
-        gender: "Female"
-    }
-]
+// Commenting this out for further exploration.
+
+// const products = [
+//     {
+//         id: 1,
+//         name: "iphone 13 pro max",
+//         category: "Electronis",
+//         price: 9900,
+//         currency: "USD",
+//         images: "",
+//         Stars: 4,
+//         review: "This is an amazing product"
+//     },
+//     {
+//         id: 2,
+//         name: "Butter Bread",
+//         category: "Food",
+//         price: 5,
+//         currency: "USD",
+//         images: "",
+//         Stars: 5,
+//         review: "This is onre of the breadon the merket"
+//     }
+// ];
+
+// const customers = [
+//     {
+//         id: 1,
+//         name: "Regan Kofi Marfo",
+//         gender: "Male"
+//     },
+//     {
+//         id: 2,
+//         name: "Kwame Nkrumah",
+//         gender: "Male"
+//     },
+//     {
+//         id: 1,
+//         name: "Yaa Asantewaa",
+//         gender: "Female"
+//     }
+// ]
 // This will print the message 
 app.get('/', function (req, res) {
     res.json({message: 'Hello World'})
